3814f3053f5e10bb0a204f34f980d2b505cdcf34
Deepak Kumar Saxena <deepzsenu@gmail.com>
2024-07-31 02:18:41 +0530

    updated final version

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
@@ -6,6 +7,7 @@ import pickle
 from surprise import SVD as SurpriseSVD, Dataset, Reader
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the trained SVD model
 with open('models/recommendation_model.pkl', 'rb') as f:
